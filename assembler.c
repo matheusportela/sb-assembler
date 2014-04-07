60e4d39a94fc34445a26cf8bf707adc8e0dde1ed
Matheus Portela <matheus.v.portela@gmail.com>
2014-04-07 00:33:01 -0300

    Improving documentation

--- assembler.c
+++ assembler.c
@@ -5,7 +5,32 @@
  *
  * @brief  Two-pass assembler for educational assembly language
  *
- * <detailed description which may contain examples and test cases>
+ * This assembler was built to assemble the language used in the Systems Software course
+ * offered at the University of Brasilia, Brazil, first semester of 2014.
+ * 
+ * The language assumes the existence of a theoretical computer, which has only one 16-bit
+ * register (named accumulator, or ACC), one 16-bit program counter (PC), 16-bit addressed
+ * memory with 216 words, and instructions in one of the following formats:
+ * Format 1: Opcode
+ * Format 2: Opcode Address
+ * Format 3: Opcode Address1 Address2
+ * 
+ * These are the available instructions, with their opcode, size (in words), and action:
+ * Name		| Opcode	| Size	| Action
+ * ADD		| 01		| 2		| ACC <- ACC + mem(OP)
+ * SUB		| 02		| 2		| ACC <- ACC - mem(OP)
+ * MUL		| 03		| 2		| ACC <- ACC * mem(OP)
+ * DIV		| 04		| 2		| ACC <- ACC / mem(OP)
+ * JMP		| 05		| 2		| PC <- OP
+ * JMPN		| 06		| 2		| If ACC < 0 then PC <- OP
+ * JMPP		| 07		| 2		| If ACC > 0 then PC <- OP
+ * JMPZ		| 08		| 2		| If ACC = 0 then PC <- OP
+ * COPY		| 09		| 3		| mem(OP2) <- mem(OP1)
+ * LOAD		| 10		| 2		| ACC <- mem(OP)
+ * STORE	| 11		| 2		| mem(OP) <- ACC
+ * INPUT	| 12		| 2		| mem(OP) <- input
+ * OUTPUT	| 13		| 2		| output <- mem(OP)
+ * STOP		| 14		| 1		| Stops execution
  */
 
 #include <stdio.h>
@@ -32,6 +57,10 @@ void assembler_second_pass(char *filename,
 						   table_t *directives_table,
 						   int program_size);
 
+/**
+ * Main function
+ * Init all required tables, run passes, and free tables.
+ */
 int main()
 {
 	table_t symbols_table;
--- directives_table.c
+++ directives_table.c
@@ -1,3 +1,13 @@
+/**
+ * @file   directives_table.c
+ * @author Matheus Vieira Portela
+ * @date   06/04/2014
+ *
+ * @brief  Implements the directives table
+ *
+ * This assembler was built to assemble the language used in the Systems Software course
+ */
+
 #include "directives_table.h"
 
 void directives_table_init(table_t *directives_table)
